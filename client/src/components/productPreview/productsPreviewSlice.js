5e2df4a0c340e716f8bc598e24f7d4d3d165c56c
Ed <76992905+edphan@users.noreply.github.com>
2021-05-08 14:02:07 -0700

    change to relative path in productsPreviewSlice.js

--- client/src/components/productPreview/productsPreviewSlice.js
+++ client/src/components/productPreview/productsPreviewSlice.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 export const loadAllProducts = createAsyncThunk('productsPreview/loadAllProducts', async () => {
-	const response = await fetch('http://localhost:5000/products/subcategory');
+	const response = await fetch('/products/subcategory');
 	const json = await response.json();
 
 	return json;
