cf2d53eb1ff5212b91f8926e48fe2bb6db59b6e2
Ed <76992905+edphan@users.noreply.github.com>
2021-05-17 17:29:00 -0700

    add bag count

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -2,24 +2,28 @@ import React from 'react';
 import logo from '../icon/apple-grey.png';
 import bag from '../icon/shopping-bag.png';
 import profile from '../icon/profile.png';
-// import search from '../icon/search.png';
 import github from '../icon/github.png';
+import { useSelector } from 'react-redux';
+import { selectBag } from './bag/bagSlice';
 
 function Nav() {
+	const productsInBag = useSelector(selectBag);
+	const quantityArray = productsInBag.map((item) => item.quantity);
+	const quantity = quantityArray.reduce((a, b) => a + b, 0);
+
 	return (
 		<div className='nav-container'>
 			<div className='nav'>
 				<a className='nav-home' href='/'>
 					<img src={logo} alt='' />
 				</a>
-				{/* <a className='nav-search' href='/'>
-					<img src={search} alt='' />
-				</a> */}
+
 				<a className='nav-profile' href='/profile'>
 					<img src={profile} alt='' />
 				</a>
 				<a className='nav-bag' href='/bag'>
 					<img src={bag} alt='' />
+					<div className={quantity === 0 ? 'bag-quantity-zero' : 'bag-quantity'}>{quantity}</div>
 				</a>
 				<a className='nav-bag' href='https://github.com/edphan'>
 					<img src={github} alt='' />
